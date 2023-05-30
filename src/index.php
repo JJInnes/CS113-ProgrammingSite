0aeb06f7cc197fc97a709c08400624445c21243b
Josh Innes <jinnes@pulsion.co.uk>
2023-05-30 14:41:26 +0100

    new design

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
     $pageHead = getComponent("./Resources/Components/Shared/pagehead.html");
 
     $header = getComponent("./Resources/Components/Shared/header.html");
-    $header = str_replace("_TITLE_", "Learn to make websites!!", $header);
+    //$header = str_replace("_TITLE_", "Learn to make websites!!", $header);
 
     $body = getComponent("./Resources/Components/indexMain.html");
 
