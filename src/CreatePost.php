f3d00b305dc4a360e218883861475b9fe3b75523
rlaiolo <reece.laiolo.2022@uni.strath.ac.uk>
2023-06-13 17:56:34 +0100

    Added Create Post page and associated PHP for entering a row into the db

---
+++ src/CreatePost.php
@@ -0,0 +1,22 @@
+<?php
+include 'Resources/helper.php';
+
+$title = $_POST['postTitle'];
+$author = $_POST['postAuthor'];
+$category = $_POST['postCategory'];
+$content = $_POST['postContent'];
+
+//echo $title;
+//echo "<br>";
+//echo $author;
+//echo "<br>";
+//echo $category;
+//echo "<br>";
+//echo $content;
+//echo "<br>";
+
+
+addPost($title,$author,$category,$content);
+
+
+?>
--- src/Resources/helper.php
+++ src/Resources/helper.php
@@ -1,4 +1,6 @@
 <?php
+
+
     function getComponent($url){
         $file = fopen($url, "r");
         $result = fread($file, filesize($url));
@@ -22,5 +24,28 @@
             echo "</body>";
 
         echo "</html>";
+    }
+
+    function addPost($title,$author,$category,$content){
+        include 'Components/PHP/ConnectionConfig.php';
+
+        $previousRowCountQuery = "SELECT * FROM CS113Proj_Posts;";
+        $previousRowCount = $conn->query($previousRowCountQuery);
+        $rowCountBefore = $previousRowCount->num_rows;
+
+        $sql = "INSERT INTO `CS113Proj_Posts` (`post_id`, `post_title`, `post_author`, `post_category`, `post_content`) VALUES (NULL, '$title', '$author', '$category', '$content');";
+
+        $result = $conn->query($sql);
+
+        $rowCountAfter = $conn->query($previousRowCountQuery);
+        $rowCountAfter = $rowCountAfter->num_rows;
+
+        if ($rowCountAfter > $rowCountBefore) {
+            echo "Row successfully inserted.";
+        } else {
+            echo "Something went wrong, no rows have been inserted";
+        }
+        $conn->close();
+
     }
 ?>
